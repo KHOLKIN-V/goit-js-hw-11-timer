3c97668146983df024c5e76cbe8e2dcf68a8753a
KHOLKIN-V <kholkinva@yandex.ru>
2021-09-12 18:29:46 +0400

    correct 1

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
-// new CountdownTimer({
-//     selector: '#timer-1',
-//     targetDate: new Date('Jul 17, 2019'),
-//   });
-
-const htmlDays = document.querySelector('span[data-value = "days"]')
-const htmlHours = document.querySelector('span[data-value = "hours"]')
-const htmlMins = document.querySelector('span[data-value = "mins"]')
-const htmlSecs = document.querySelector('span[data-value = "secs"]')
-
 class CountdownTimer {
     constructor(opts) {
         this.selector = opts.selector;
         this.targetDate = opts.targetDate;
     }
+    pad(i) {
+        return String(i).padStart(2, '0');
+    };
+    getTimeComps(j) {
+        const days = this.pad(Math.floor(j / (1000 * 60 * 60 * 24)));
+        const hours = this.pad(Math.floor((j % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        const mins = this.pad(Math.floor((j % (1000 * 60 * 60)) / (1000 * 60)));
+        const secs = this.pad(Math.floor((j % (1000 * 60)) / 1000));
+    
+        return { days, hours, mins, secs };
+    }
     start () {
         setInterval(() => {
+            const htmlDays = document.querySelector('span[data-value = "days"]')
+            const htmlHours = document.querySelector('span[data-value = "hours"]')
+            const htmlMins = document.querySelector('span[data-value = "mins"]')
+            const htmlSecs = document.querySelector('span[data-value = "secs"]')
+        
             const currentTime = Date.now();
             const time = this.targetDate - currentTime;
-            const { days, hours, mins, secs } = getTimeComps(time);
+            const { days, hours, mins, secs } = this.getTimeComps(time);
             console.log(`${days}:${hours}:${mins}:${secs}`);
 
             htmlDays.textContent = `${days}`;
@@ -34,15 +40,3 @@ const timer = new CountdownTimer({
       });
 
 timer.start();
-
-function pad(i) {
-    return String(i).padStart(2, '0');
-};
-function getTimeComps(j) {
-    const days = pad(Math.floor(j / (1000 * 60 * 60 * 24)));
-    const hours = pad(Math.floor((j % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-    const mins = pad(Math.floor((j % (1000 * 60 * 60)) / (1000 * 60)));
-    const secs = pad(Math.floor((j % (1000 * 60)) / 1000));
-
-    return { days, hours, mins, secs };
-}
